c20afab9629862c8e1ab7e723373dcf3914168de
Aman Pal Singh <APS@macoss-MacBook-Pro.local>
2024-09-20 13:39:51 +0530

    kept the master branch as it was i.e. questions only

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,34 +5,7 @@
 */
 
 function isAnagram(str1, str2) {
-  str1 = str1.toLowerCase();
-  str2 = str2.toLowerCase();
-  let str1Obj = {};
-  let str2Obj = {};
-  if(str1.length != str2.length){
-    return false;
-  }
-  for(let i = 0; i <str1.length;i++){
-      if(str1Obj[str1[i]] == undefined){
-        str1Obj[str1[i]] = 1;
-      }else
-      str1Obj[str1[i]] += 1;
-    }
-    for(let i = 0; i <str1.length;i++){
-      if(str2Obj[str2[i]] == undefined){
-        str2Obj[str2[i]] = 1;
-      }else
-      str2Obj[str2[i]] += 1;
-    }
-    console.log(str1Obj);
-    console.log(str2Obj);
-    for(let key in str1Obj){
-       if(!str2Obj.hasOwnProperty(key) || str1Obj[key] != str2Obj[key]){
-        return false
-       }
-    }
-    return true;
+
 }
 
 module.exports = isAnagram;
-//isAnagram("Debit Card", "Bad Credit");
